8b2fb3950651e009992a35993361220e964a925a
sergey2012sergey <wotru0@gmail.com>
2023-01-21 18:42:54 +0200

    print_graph function, reading the file, creating nodes, print not working properly

--- src/ExManager.java
+++ src/ExManager.java
@@ -1,14 +1,28 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+
 public class ExManager {
     private String path;
-    private num_of_nodes;
+    private int num_of_nodes;
+    private List<Pair<Integer,Node>> nodes;
     // your code here
 
     public ExManager(String path){
         this.path = path;
+        this.nodes = new ArrayList<>();
         // your code here
     }
-    public Node getNode(int id){
+    public Node get_node(int id){
         // your code here
+        for (Pair<Integer, Node> pair : nodes) {
+            if (pair.getKey() == id) {
+                return pair.getValue();
+            }
+        }
+        return null;
     }
 
     public int getNum_of_nodes() {
@@ -19,8 +33,57 @@ public class ExManager {
         //your code here
     }
 
-    public void read_txt(){
+    public void read_txt() throws FileNotFoundException{
         // your code here
+        System.out.println(this.path);
+        Scanner scanner = new Scanner(new File(this.path));
+        String line = scanner.nextLine();
+        num_of_nodes = Integer.parseInt(line);
+        System.out.println(num_of_nodes+"this is the number of nodes");
+        while(scanner.hasNextLine()) {
+            line = scanner.nextLine();
+            if(line.contains("stop")){
+                break;
+            }
+            System.out.println(line);
+            String[] words = line.split("\\s+");
+
+            System.out.println(words[0] + "this is the node");
+            int nodeId = Integer.parseInt(words[0]);
+            List<Integer> neighborIds = new ArrayList<>();
+            ArrayList<Pair<Integer, List<Integer>>> PortUsage = new ArrayList<>();
+            List<Pair<Integer,Float>> EdgeWeight = new ArrayList<>();
+
+            for(int i = 1; i < words.length; i++){
+
+                if((i) % 4 ==1) {
+                    System.out.println(words[i] + "this is the i neighbor");
+                    neighborIds.add(Integer.parseInt(words[i]));
+                }
+                if((i) % 4 ==2) {
+                    System.out.println(words[i] + "this is the wight");
+                    EdgeWeight.add(new Pair<>(Integer.parseInt(words[i-1]),Float.parseFloat(words[i])));
+                }
+                if((i) % 4 ==3) {
+                    System.out.println(words[i] + "this is the port1");
+                }
+                if((i) % 4 ==0) {
+                    System.out.println(words[i] + "this is the port2");
+                    List<Integer> ports = new ArrayList<>();
+                    ports.add(Integer.parseInt(words[i-1]));
+                    ports.add(Integer.parseInt(words[i]));
+                    PortUsage.add(new Pair<>(Integer.parseInt(words[i-3]), ports));
+                }
+            }
+            System.out.println(neighborIds+"111111111111111111");
+            System.out.println(EdgeWeight+"2222222222222222");
+            System.out.println(PortUsage+"33333333333");
+            System.out.println(nodeId+ " 0000000");
+            System.out.println(num_of_nodes+ " 0000000");
+            Node node1 = new Node(nodeId,num_of_nodes, neighborIds, EdgeWeight,PortUsage);
+            System.out.println(node1);
+            nodes.add(new Pair<>(nodeId,node1));
+        }
     }
 
     public void start(){
--- src/Node.java
+++ src/Node.java
@@ -2,19 +2,22 @@ import java.util.*;
 
 public class Node extends Thread{
         private final int id;
-        private List<Node> ConnectedNodes;
-        private List<Pair<Integer,Float>> EdgeWeight;
+//        private List<Node> ConnectedNodes;
+        private List<Pair<Integer,Float>> EdgeWeight; //(key,value)=(neighbour_id,weight)
         private ArrayList<Pair<Integer,List<Integer>>> PortUsage ;//(key,value)=(neighbour_id,(send_port,listen_port))
         private List<Integer> neighborIds;
         private final int NumberOfNodes;
-        private static float [][]  adjencyList;
+        private static float [][] adjacencyList;
 
-        public Node(int id , int NumberOfNodes) {
+        public Node(int id , int NumberOfNodes, List<Integer> neighborIds, List<Pair<Integer,Float>> EdgeWeight,
+                    ArrayList<Pair<Integer,List<Integer>>> PortUsage) {
             super();
             this.id = id;
             this.NumberOfNodes = NumberOfNodes;
-            ConnectedNodes = new ArrayList<>();
-
+            this.neighborIds = neighborIds;
+            this.EdgeWeight = EdgeWeight;
+            this.PortUsage = PortUsage;
+            this.adjacencyList = new float[NumberOfNodes][NumberOfNodes];
         }
         @Override
         public void run(){
@@ -22,30 +25,59 @@ public class Node extends Thread{
         }
 
         public long getId() {
+
             return id;
         }
 
-        public Node getNode() {return }
+        private void updateAdjacencyList(){
+            for (int i = 0; i < NumberOfNodes; i++) {
+                for(int j = 0; j <NumberOfNodes; j++) {
+//                    adjacencyList[i][j] = 1;
+                    if (i == j){
+                        adjacencyList[i][j] = -1;
+                    }
+                    else{
+                        if (neighborIds.contains(j) && id == i+1){
+                        for (Pair<Integer, Float> pair : EdgeWeight) {
+                            if (pair.getKey() == i) {
+                                adjacencyList[i][j] = 55;
+                            }else{
+                                adjacencyList[i][j] = 6666;
+                            }
+                        }
+                    } else {
+                        adjacencyList[i][j] = -1;
+                    }
+                    }
+                }
+            }
+        }
 
         public List<Integer> getNeighborIds() {
+
             return neighborIds;
         }
         // done maybe working
         public void print_graph(){
+            updateAdjacencyList();
+            System.out.println("printin G " + id);
+//            System.out.println(adjacencyList[0][0]+" 88888");
             // outer loop
-            for (int i = 1; i <= NumberOfNodes; ++i) {
+            for (int i = 0; i < NumberOfNodes; i++) {
                 // codes
-                System.out.print(i);
+                System.out.print(i+1 + " ");
                 // inner loop
-                for(int j = 1; j <=NumberOfNodes; ++j) {
-                    if (adjencyList[i][j] != -1 ){
-                        System.out.print(j);
-                        System.out.print(adjencyList[i][j]);
-                        System.out.print(PortUsage.get(j).getValue().get(0));
-                        System.out.print(PortUsage.get(j).getValue().get(1));
-                    }
-                    System.out.println("");
+                for(int j = 0; j < NumberOfNodes; j++) {
+                    System.out.print(adjacencyList[i][j]+" ");
+//                    if (adjacencyList[i][j] != -1 ){
+//                        System.out.print(j+1+" ");
+//                        System.out.print(adjacencyList[i][j]+" ");
+//                        System.out.print(PortUsage.get(j).getValue().get(0)+" ");
+//                        System.out.print(PortUsage.get(j).getValue().get(1)+" ");
+//                    }
+//
                 }
+                System.out.println("");
             }
         }
     }
